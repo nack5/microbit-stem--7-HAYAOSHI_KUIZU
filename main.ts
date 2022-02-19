eaf7ec68d80f5f57225261473a2be908d38045b6
nack5 <100018350+nack5@users.noreply.github.com>
2022-02-19 18:38:45 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,20 @@
+basic.forever(function () {
+    if (input.pinIsPressed(TouchPin.P0)) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    if (input.pinIsPressed(TouchPin.P1)) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+})
